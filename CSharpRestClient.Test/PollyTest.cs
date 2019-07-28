efdb023e6245659f5d77e281ae1c3d1a758e1834
Wesley Egberto <wesleyegberto@gmail.com>
2019-07-28 06:00:13 -0300

    ajuste do teste

--- CSharpRestClient.Test/PollyTest.cs
+++ CSharpRestClient.Test/PollyTest.cs
@@ -7,6 +7,8 @@ using Xunit;
 
 namespace CSharpRestClient.Test {
     public class PollyTest {
+        private const string BASE_URL = "http://httpbin.org";
+
         [Fact]
         public void TestPolly() {
             var policy = Policy
@@ -15,30 +17,31 @@ namespace CSharpRestClient.Test {
                     Console.WriteLine("Exception thrown: " + retryCount);
                 })
                 .ExecuteAndCapture(() => {
-                    throw new Exception("Exception to capture");
                     return 42;
+                    throw new Exception("Exception to capture");
                 });
 
             Assert.Null(policy.FinalException);
-            Assert.Equal(10, policy.Result);
+            Assert.Equal(42, policy.Result);
         }
 
         public ModelWithGuid fallback() {
             return new ModelWithGuid() {
-                Uuid = Guid.Empty
+                Uuid = Guid.Parse("b6fca409-12fa-4b09-8c67-29699e286871")
             };
         }
 
         [Fact]
         public async Task Should_Request_Toxy_Server_With_Policy() {
-            var model = await HttpClientBuilder.Create("http://localhost:3000/uuid")
+            var model = await HttpClientBuilder.Create("http://invalid.url")
+                .Path("uuid")
                 .AsyncGet<ModelWithGuid>()
                 .Retry(2)
                 .Fallback(async can => fallback())
                 .GetEntity();
 
             Assert.NotNull(model);
-            Assert.NotEqual(Guid.Empty, model.Uuid);
+            Assert.Equal(Guid.Parse("b6fca409-12fa-4b09-8c67-29699e286871"), model.Uuid);
         }
     }
 }
